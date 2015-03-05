9bddc7d16b6d49461c1158a663b3a0ca69f62a93
Avery Brooks <averyjamesbrooks@gmail.com>
2015-03-05 11:01:19 -0500

    tab

--- make.php
+++ make.php
@@ -10,19 +10,21 @@ $files = scandir($tardir, SCANDIR_SORT_ASCENDING);
 
 for($l = 0; $l < 10; $l++) {
 
-foreach($files as $file) {
+	foreach($files as $file) {
 
-	if (substr($file,-3) == "jpg") {
+		if (substr($file,-3) == "jpg") {
 
-		$loops = 10;
+			$loops = 10;
 
-		for($i = 0; $i < $loops; $i++) {
+			for($i = 0; $i < $loops; $i++) {
 
-			$quality = rand(1,100);
+				$quality = rand(1,100);
 
-			echo "Processing $tardir/$file to $quality\n";
-			$im = imagecreatefromjpeg("$tardir/$file");
-			imagejpeg($im, "$tardir/$file", $quality);
+				echo "Processing $tardir/$file to $quality\n";
+				$im = imagecreatefromjpeg("$tardir/$file");
+				imagejpeg($im, "$tardir/$file", $quality);
+
+			}
 
 		}
 
@@ -30,9 +32,6 @@ foreach($files as $file) {
 
 }
 
-}
-
 `ffmpeg -i $tardir/%6d.jpg -vf fps=24 $tardir/out.mp4`;
 
 `open $tardir/out.mp4`;
-
